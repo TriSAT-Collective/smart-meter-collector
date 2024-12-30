785ea807a2b78a1a2d41e6b49efb6ad06aeaa075
NINNiT <soelderer@gmail.com>
2024-12-30 15:29:57 +0100

    feat: refactor various things

---
+++ trisatenergy_SMCollector/SmartMeterCollector/SmartMeterResultPayload.cs
@@ -0,0 +1,17 @@
+namespace trisatenergy_smartmeters.SmartMeterSimulation;
+
+public enum EnergySourceType
+{
+    Solar,
+    Wind,
+    Other
+}
+
+public class SmartMeterResultPayload
+{
+    public DateTime Timestamp { get; set; }
+    public double TotalProduction { get; set; }
+    public Dictionary<EnergySourceType, double> ProductionBySource { get; set; }
+    public double TotalConsumption { get; set; }
+    public bool MaintenanceMode { get; set; }
+}
