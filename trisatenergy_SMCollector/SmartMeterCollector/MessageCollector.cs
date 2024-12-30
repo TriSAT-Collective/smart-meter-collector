a1367eddf5f2c809cd600eae3b1068934bb59490
NINNiT <soelderer@gmail.com>
2024-12-30 13:58:49 +0100

    feat: remove IAsyncDisposable

--- trisatenergy_SMCollector/SmartMeterCollector/MessageCollector.cs
+++ trisatenergy_SMCollector/SmartMeterCollector/MessageCollector.cs
@@ -4,64 +4,57 @@ using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-namespace trisatenergy_SMCollector.SmartMeterCollector
+namespace trisatenergy_SMCollector.SmartMeterCollector;
+
+public class MessageCollector
 {
-    public class MessageCollector : IAsyncDisposable
+    private readonly ILogger<MessageCollector> _logger;
+    private readonly AppSettings _settings;
+
+    private IChannel _channel; // Changed from IModel to IChannel
+    private IConnection _connection;
+
+    public MessageCollector(IOptions<AppSettings> settings, ILogger<MessageCollector> logger)
     {
-        
-        private readonly ILogger<MessageCollector> _logger;
-        private readonly AppSettings _settings;
-        
-        private IConnection _connection;
-        private IChannel _channel; // Changed from IModel to IChannel
+        _settings = settings.Value;
+        _logger = logger;
+    }
 
-        public MessageCollector(IOptions<AppSettings> settings, ILogger<MessageCollector> logger)
+    public async Task StartCollectingAsync()
+    {
+        _logger.LogInformation("Starting to collect messages...");
+        // Setup RabbitMQ connection and channel
+        var factory = new ConnectionFactory
         {
-            _settings = settings.Value;
-            _logger = logger;
-        }
+            Uri = _settings.RabbitMQ.Uri,
+            VirtualHost = _settings.RabbitMQ.VirtualHost,
+            UserName = _settings.RabbitMQ.Username,
+            Password = _settings.RabbitMQ.Password
+        };
 
-        public async Task InitializeAsync()
+        // Asynchronously create the connection and channel
+        _connection = await factory.CreateConnectionAsync();
+        _channel = await _connection.CreateChannelAsync();
+
+        // Define the consumer event
+        var consumer = new AsyncEventingBasicConsumer(_channel);
+        consumer.ReceivedAsync += (model, ea) =>
         {
-            // Setup RabbitMQ connection and channel
-            var factory = new ConnectionFactory()
-            {
-                Uri = _settings.RabbitMQ.Uri,
-                VirtualHost =   _settings.RabbitMQ.VirtualHost,
-                UserName = _settings.RabbitMQ.Username,
-                Password = _settings.RabbitMQ.Password
-            };
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            _logger.LogInformation($"Received message: {message}");
+            return Task.CompletedTask;
+        };
 
-            // Asynchronously create the connection and channel
-            _connection = await factory.CreateConnectionAsync();
-            _channel = await _connection.CreateChannelAsync();  
-        }
+        // Start consuming messages from the specified "OK" queue
+        await _channel.BasicConsumeAsync(_settings.RabbitMQ.QueueName, true, consumer);
+    }
 
-        public async Task StartCollectingAsync()
-        {
-
-            await InitializeAsync();
-            
-            // Define the consumer event
-            var consumer = new AsyncEventingBasicConsumer(_channel);
-            consumer.ReceivedAsync += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                _logger.LogInformation($"Received message: {message}");
-                return Task.CompletedTask;
-            };
-
-            // Start consuming messages from the specified "OK" queue
-            await _channel.BasicConsumeAsync(queue: _settings.RabbitMQ.QueueName, autoAck: true, consumer: consumer);
-        }
-
-        public async ValueTask DisposeAsync()
-        {
-            await _channel.CloseAsync();
-            await _channel.DisposeAsync();
-            await _connection.CloseAsync();
-            await _connection.DisposeAsync();
-        }
+    public async ValueTask DisposeAsync()
+    {
+        await _channel.CloseAsync();
+        await _channel.DisposeAsync();
+        await _connection.CloseAsync();
+        await _connection.DisposeAsync();
     }
 }
